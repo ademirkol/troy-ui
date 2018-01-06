fd8913c05b94c251a77a58571feb97e504036927
ademirkol <demirkol.anil@hotmail.com>
2018-01-06 17:37:30 +0300

    Configuration and example components for doc generator

--- src/components/HelloWorld/HelloWorld.js
+++ src/components/HelloWorld/HelloWorld.js
@@ -1,11 +1,18 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
+/** Hello World React reusable components */
+
 function HelloWorld({message}) {
     return <div>Hello {message}</div>
 }
 HelloWorld.propTypes = {
+    /** Message to display */
   message:PropTypes.string
+};
+
+HelloWorld.defaultProps = {
+    message:"World"
 };
 
 export default HelloWorld;
---
+++ src/docs/examples/HelloWorld/ExampleHelloWorld.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import HelloWorld from 'troy-ui/HelloWorld'
+
+/** Code Example */
+export default function ExampleHelloWorld() {
+    return <HelloWorld message={"Test"}/>
+
+}
